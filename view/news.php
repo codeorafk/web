b04b799971d102681875bca97aac633078b3bfc0
Tryhard789 <85111677+Tryhard789@users.noreply.github.com>
2021-11-28 17:52:18 +0700

    Add files via upload

---
+++ view/news.php
@@ -0,0 +1,65 @@
+
+   <?php include('partials-front/menu.php'); ?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<section class="news">
+    <div class="container">
+      <div class="logo-wrapper d-flex align-items-center">
+        <h1><a href="">The News</a></h1>
+      </div>
+    </div>
+
+    <div class="container">
+    <div class="row">
+        <div class="card col-4" style="width: 350px;">
+            <img class="card-img-top" src="../public/images/news1.jpg" alt="Card image cap"style="width: 350px;">
+            <div class="card-body">
+            <a href="https://www.delish.com/food-news/a29797224/taco-bell-thanksgiving-hours/" class="card-title">Is Taco Bell Open On Thanksgiving Day?</a>
+            <p class="card-text">Some things in this world are largely disappointing, i.e. the supermarket being sold out of special-edition sweets, ice cream falling on the ground,</p>
+            </div>
+        </div>
+        <div class="card col-4 offset-1" style="width: 350px;">
+            <img class="card-img-top" src="../public/images/new2.jpg" alt="Card image cap">
+            <div class="card-body">
+            <a href="" class="card-title">All The Grocery Stores Open On Thanksgiving Day</a>
+            <p class="card-text">You know what happens to the best-laid plans, right? They go awry. Meaning that Thanksgiving shopping list you triple-checked actually...</p>
+            </div>
+        </div>
+    </div>    
+    <br>
+    <div class="row">
+        <div class="card col-4" style="width: 350px;">
+            <img class="card-img-top" src="../public/images/news3.jpg" alt="Card image cap">
+            <div class="card-body">
+            <a href="" class="card-title">Is McDonald's Open On Thanksgiving?</a>
+            <p class="card-text">Maybe you're a pro cook in the kitchen, especially when it comes to food-centric holidays like Thanksgiving...</p>
+            </div>
+        </div>
+        <div class="card col-4 offset-1" style="width: 350px;">
+            <img class="card-img-top" src="../public/images/news4.jpeg" alt="Card image cap">
+            <div class="card-body">
+            <a href="" class="card-title">Cracker Barrel Is Selling Thanksgiving Dinner</a>
+            <p class="card-text">Sure, it's true that Thanksgiving is all about food, but that doesn't mean that you have to be cooking everything yourself...</p>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="card col-4" style="width: 350px;">
+            <img class="card-img-top" src="../public/images/news5.png" alt="Card image cap">
+            <div class="card-body">
+            <a href="" class="card-title">Here's Where to Buy Thanksgiving Dinner</a>
+            <p class="card-text">According to research conducted by Bob Evans Restaurants,...</p>
+            </div>
+        </div>
+     
+        <div class="card col-4 offset-1" style="width: 350px;">
+            <img class="card-img-top" src="../public/images/new6.jpg" alt="Card image cap">
+            <div class="card-body">
+            <a href="" class="card-title">Score Deals On Mini Fridges This Black Friday</a>
+            <p class="card-text">We've all played an intricate game of Jenga with the groceries inside our refrigerators at one point or another...</p>
+            </div>
+        </div>
+    </div>    
+    </div>
+</section>
+<?php include('partials-front/footer.php'); ?>
