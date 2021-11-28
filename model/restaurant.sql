66ee141ecd9c6376ced1f8e7e8cd4fd618016189
codeorafk <63441848+codeorafk@users.noreply.github.com>
2021-11-29 04:39:23 +0700

    change db

--- model/restaurant.sql
+++ model/restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:8111
--- Generation Time: Nov 28, 2021 at 10:56 AM
+-- Generation Time: Nov 28, 2021 at 10:38 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -63,11 +63,11 @@ CREATE TABLE `tbl_category` (
 --
 
 INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
-(4, 'Pizza', 'Food_Category_790.jpg', 'Yes', 'Yes'),
+(4, 'Pizza', 'Food_Category_563.jpg', 'Yes', 'Yes'),
 (5, 'Burger', 'Food_Category_344.jpg', 'Yes', 'Yes'),
 (6, 'MoMo', 'Food_Category_77.jpg', 'Yes', 'Yes'),
-(8, 'Quia est ipsum id id', 'Food_Category_929.jpg', 'No', 'Yes'),
-(9, 'Chicken', 'Food_Category_690.tmp', 'Yes', 'Yes');
+(12, 'default', 'default.jpg', 'Yes', 'No'),
+(13, 'Chicken', 'Food_Category_594.tmp', 'Yes', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -91,12 +91,30 @@ CREATE TABLE `tbl_food` (
 --
 
 INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
-(3, 'Dumplings Specials', 'Chicken Dumpling with herbs from Mountains', '5.00', 'Food-Name-3649.jpg', 6, 'Yes', 'Yes'),
-(4, 'Best Burger', 'Burger with Ham, Pineapple and lots of Cheese.', '4.00', 'Food-Name-6340.jpg', 5, 'Yes', 'Yes'),
-(5, 'Smoky BBQ Pizza', 'Best Firewood Pizza in Town.', '6.00', 'Food-Name-8298.jpg', 4, 'No', 'Yes'),
-(6, 'Sadeko Momo', 'Best Spicy Momo for Winter', '6.00', 'Food-Name-7387.jpg', 6, 'Yes', 'Yes'),
+(5, 'Best chicken', 'Best Firewood Pizza in Town.', '6.00', 'Food-Name-943.jpg', 13, 'No', 'Yes'),
+(6, 'Sadeko Momo', 'Best Spicy Momo for Winter', '6.00', 'Food-Name-7387.jpg', 8, 'Yes', 'Yes'),
 (7, 'Mixed Pizza', 'Pizza with chicken, Ham, Buff, Mushroom and Vegetables', '10.00', 'Food-Name-7833.jpg', 4, 'Yes', 'Yes'),
-(8, 'Sed ipsum cillum in', 'Sed aut officiis qui', '52.00', '', 5, 'No', 'No');
+(9, 'chicken wing', 'ngon, giòn, béo', '12.00', 'Food-Name-830.jpg', 13, 'Yes', 'Yes'),
+(24, 'Chicken', 'ngon', '10.00', 'Food_Name_756.tmp', 13, 'Yes', 'Yes'),
+(25, 'Chicken', 'ngon', '10.00', 'Food_Name_331.tmp', 13, 'Yes', 'Yes'),
+(26, 'Chicken', 'ngon', '10.00', 'Food_Name_28.tmp', 13, 'Yes', 'Yes'),
+(27, 'Chicken', 'ngon', '10.00', 'Food_Name_16.tmp', 13, 'Yes', 'Yes'),
+(28, 'Chicken', 'ngon', '10.00', 'Food_Name_866.tmp', 13, 'Yes', 'Yes'),
+(29, 'Chicken', 'ngon', '10.00', 'Food_Name_76.tmp', 13, 'Yes', 'Yes'),
+(30, 'Chicken', 'ngon', '10.00', 'Food_Name_640.tmp', 13, 'Yes', 'Yes'),
+(31, 'Chicken', 'ngon', '10.00', 'Food_Name_308.tmp', 13, 'Yes', 'Yes'),
+(32, 'Chicken', 'ngon', '10.00', 'Food_Name_415.tmp', 13, 'Yes', 'Yes'),
+(33, 'Chicken', 'ngon', '10.00', 'Food_Name_266.tmp', 13, 'Yes', 'Yes'),
+(34, 'Chicken', 'ngon', '10.00', 'Food_Name_998.tmp', 13, 'Yes', 'Yes'),
+(35, 'Chicken', 'ngon', '10.00', 'Food_Name_988.tmp', 13, 'Yes', 'Yes'),
+(36, 'Chicken', 'ngon', '10.00', 'Food_Name_27.tmp', 13, 'Yes', 'Yes'),
+(37, 'Chicken', 'ngon', '10.00', 'Food_Name_280.tmp', 13, 'Yes', 'Yes'),
+(38, 'Chicken', 'ngon', '10.00', 'Food_Name_928.tmp', 13, 'Yes', 'Yes'),
+(39, 'Chicken', 'ngon', '10.00', 'Food_Name_608.tmp', 13, 'Yes', 'Yes'),
+(40, 'Chicken', 'ngon', '10.00', 'Food_Name_915.tmp', 13, 'Yes', 'Yes'),
+(41, 'Chicken', 'ngon', '10.00', 'Food_Name_850.tmp', 13, 'Yes', 'Yes'),
+(42, 'Chicken', 'ngon', '10.00', 'Food_Name_261.tmp', 13, 'Yes', 'Yes'),
+(43, 'Chicken', 'ngon', '10.00', 'Food_Name_539.tmp', 13, 'Yes', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -123,7 +141,101 @@ INSERT INTO `tbl_food_in_order` (`id`, `food_id`, `order_id`, `quantity`) VALUES
 (5, 5, 9, 2),
 (6, 7, 9, 2),
 (7, 4, 9, 2),
-(8, 3, 9, 1);
+(8, 3, 9, 1),
+(9, 5, 10, 3),
+(10, 7, 10, 3),
+(11, 4, 10, 2),
+(12, 3, 10, 1),
+(13, 5, 11, 1),
+(14, 5, 12, 1),
+(15, 3, 12, 1),
+(16, 4, 12, 1),
+(17, 7, 12, 1),
+(18, 6, 12, 1),
+(19, 5, 13, 1),
+(20, 3, 13, 2),
+(21, 4, 13, 1),
+(22, 7, 13, 1),
+(23, 6, 13, 1),
+(24, 5, 14, 1),
+(25, 3, 14, 3),
+(26, 4, 14, 2),
+(27, 7, 14, 2),
+(28, 6, 14, 1),
+(29, 3, 15, 2),
+(30, 6, 15, 1),
+(31, 7, 15, 1),
+(32, 4, 15, 1),
+(33, 3, 16, 2),
+(34, 6, 16, 1),
+(35, 7, 16, 1),
+(36, 4, 16, 1),
+(37, 3, 17, 2),
+(38, 6, 17, 1),
+(39, 7, 17, 1),
+(40, 4, 17, 1),
+(41, 3, 18, 2),
+(42, 6, 18, 1),
+(43, 7, 18, 1),
+(44, 4, 18, 1),
+(45, 6, 19, 1),
+(46, 3, 19, 1),
+(47, 6, 20, 1),
+(48, 3, 20, 1),
+(49, 7, 21, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_guess`
+--
+
+CREATE TABLE `tbl_guess` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `full_name` varchar(150) CHARACTER SET armscii8 NOT NULL,
+  `email` varchar(150) CHARACTER SET armscii8 NOT NULL,
+  `username` varchar(150) CHARACTER SET armscii8 NOT NULL,
+  `address` varchar(255) CHARACTER SET armscii8 NOT NULL,
+  `password` varchar(255) CHARACTER SET armscii8 NOT NULL,
+  `phone` varchar(20) CHARACTER SET armscii8 NOT NULL,
+  `status` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_guess`
+--
+
+INSERT INTO `tbl_guess` (`id`, `full_name`, `email`, `username`, `address`, `password`, `phone`, `status`) VALUES
+(1, 'Nguyen', 'Uy', 'uy', 'Uowy', '21232f297a57a5a743894a0e4a801fc3', '0708035130', 'Active'),
+(2, 'Luan', 'luan@bk.co', 'luan', 'Luan\'s address', 'dd960d42bb47da21af3b3b0c31684540', '0708050102', 'Banning'),
+(3, 'Luu', 'luu@bk.co', 'luu', 'luwu @ uwu ', 'b082b04c7a891a7f67d2400c2688fa06', '0123456789', 'Sleeping'),
+(4, 'Khoa', 'khoa@bk.co', 'khoa', 'khoa khooa khhoa kkhoa khoaa', 'e8ed38edd226e6d0bb0f65e07d8f3f55', '0123452785', 'Banning'),
+(7, 'Another Uy', 'uy@gma.com', 'uy1', '06 Quang Trung P Binh Dinh', '28a91aaba26255e090f348abc10f0890', '0123456789', 'Sleeping'),
+(8, 'Another Uy', 'uy@gma.com', 'uy2', '06 Quang Trung P Binh Dinh', 'f341e5874ffac9e9890262b200beb240', '0123456789', 'Sleeping'),
+(9, 'another luu', 'luu@luu.com', 'luu1', '06 Quang Trung P Binh Dinh', '45e8f18f10678960c115928a27c03c8e', '0123456789', 'Unactive');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_news`
+--
+
+CREATE TABLE `tbl_news` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `description` longtext NOT NULL,
+  `title` text NOT NULL,
+  `image_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_news`
+--
+
+INSERT INTO `tbl_news` (`id`, `description`, `title`, `image_name`) VALUES
+(1, 'Some things in this world are largely disappointing, i.e. the supermarket being sold out of special-edition sweets, ice cream falling on the ground, spaghetti sauce on a white shirt. And Im sorry to trigger that disappointment once again. Get this: Taco Bell, the iconic fast food haven, is CLOSED on Thanksgiving Day. That means if you want to eat delicious tacos and nachos on Thanksgiving, you will have to make them yourself. Yeah, that is a scary thought. Seriously though, we’ll need a minute to gather ourselves. After all, Taco Bell has been there for us through thick and thin, drunk eats and nacho cravings galore. And now, burritos and tacos are deserting us in our moment of need. Because let’s face it: We’ll inevitably overcook the Thanksgiving turkey. Sigh. But everyone deserves a day off, especially the angels who craft Doritos Locos Tacos. Tears aside, not all hope should be lost. While the chain is traditionally closed for Thanksgiving (double check with your local store to see if its hours are different—it is also a good idea to check in re: their most up-to-date COVID-19 safety protocols), the restaurant will open its doors once again starting on Black Friday. You know where to find us on November 26 (...at the Taco Bell drive-thru in case that was not clear)', 'Taco Bell For Thanksgiving Is A Major Mood-But Is It Open?', 'news1.jpg'),
+(2, 'You know what happens to the best-laid plans, right? They go awry. Meaning that Thanksgiving shopping list you triple-checked actually had something missing—and you probably will not notice it until the morning of the holiday. Luckily, a whole slew of supermarket chains have bet on the fact that you will do just that, and they are keeping their doors open for last-minute purchases. Hours are subject to change regionally, especially given the ongoing COVID-19 pandemic, so be sure to call your closest location before running out. Here is a general guide to the grocery stores you can count on come Thanksgiving day—and the ones that will not be open, including some that are closing down for the holiday for the first time in years. P.S. Not up for cooking this year? These fast food joints will stay open on Thanksgiving, and you can sit down for a meal at these restaurants on Turkey Day, too.', 'All The Grocery Stores Open On Thanksgiving Day, Because You Know You are Going To Forget Something', 'new2.jpg'),
+(3, 'Some things in this world are largely disappointing, i.e. the supermarket being sold out of special-edition sweets, ice cream falling on the ground, spaghetti sauce on a white shirt. And Im sorry to trigger that disappointment once again. Get this: Taco Bell, the iconic fast food haven, is CLOSED on Thanksgiving Day. That means if you want to eat delicious tacos and nachos on Thanksgiving, you will have to make them yourself. Yeah, that is a scary thought. Seriously though, we’ll need a minute to gather ourselves. After all, Taco Bell has been there for us through thick and thin, drunk eats and nacho cravings galore. And now, burritos and tacos are deserting us in our moment of need. Because let’s face it: We’ll inevitably overcook the Thanksgiving turkey. Sigh. But everyone deserves a day off, especially the angels who craft Doritos Locos Tacos. Tears aside, not all hope should be lost. While the chain is traditionally closed for Thanksgiving (double check with your local store to see if its hours are different—it is also a good idea to check in re: their most up-to-date COVID-19 safety protocols), the restaurant will open its doors once again starting on Black Friday. You know where to find us on November 26 (...at the Taco Bell drive-thru in case that was not clear)', 'Taco Bell For Thanksgiving Is A Major Mood-But Is It Open?', 'news1.jpg'),
+(4, 'You know what happens to the best-laid plans, right? They go awry. Meaning that Thanksgiving shopping list you triple-checked actually had something missing—and you probably will not notice it until the morning of the holiday. Luckily, a whole slew of supermarket chains have bet on the fact that you will do just that, and they are keeping their doors open for last-minute purchases. Hours are subject to change regionally, especially given the ongoing COVID-19 pandemic, so be sure to call your closest location before running out. Here is a general guide to the grocery stores you can count on come Thanksgiving day—and the ones that will not be open, including some that are closing down for the holiday for the first time in years. P.S. Not up for cooking this year? These fast food joints will stay open on Thanksgiving, and you can sit down for a meal at these restaurants on Turkey Day, too.', 'All The Grocery Stores Open On Thanksgiving Day, Because You Know You are Going To Forget Something', 'new2.jpg');
 
 -- --------------------------------------------------------
 
@@ -147,15 +259,27 @@ CREATE TABLE `tbl_order` (
 --
 
 INSERT INTO `tbl_order` (`id`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`) VALUES
-(1, '18.00', '2020-11-30 03:49:48', 'Cancelled', 'Bradley Farrell', '+1 (576) 504-4657', 'zuhafiq@mailinator.com', 'Duis aliqua Qui lor'),
-(2, '16.00', '2020-11-30 03:52:43', 'Delivered', 'Kelly Dillard', '+1 (908) 914-3106', 'fexekihor@mailinator.com', 'Incidunt ipsum ad d'),
-(3, '20.00', '2020-11-30 04:07:17', 'Delivered', 'Jana Bush', '+1 (562) 101-2028', 'tydujy@mailinator.com', 'Minima iure ducimus'),
-(4, '0.00', '0000-00-00 00:00:00', 'Đang xử lí', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
-(5, '0.00', '0000-00-00 00:00:00', 'Đang xử lí', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
-(6, '0.00', '2021-11-28 10:25:42', 'Đang xử lí', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
-(7, '0.00', '2021-11-28 10:43:15', 'Đang xử lí', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
-(8, '45.00', '2021-11-28 10:48:27', 'Đang xử lí', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
-(9, '45.00', '2021-11-28 10:50:51', 'Đang xử lí', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung');
+(1, '18.00', '2020-11-30 03:49:48', 'Completed', 'Bradley Farrell', '+1 (576) 504-4657', 'zuhafiq@mailinator.com', 'Duis aliqua Qui lor'),
+(2, '16.00', '2020-11-30 03:52:43', 'Completed', 'Kelly Dillard', '+1 (908) 914-3106', 'fexekihor@mailinator.com', 'Incidunt ipsum ad d'),
+(3, '20.00', '2020-11-30 04:07:17', 'Completed', 'Jana Bush', '+1 (562) 101-2028', 'tydujy@mailinator.com', 'Minima iure ducimus'),
+(4, '0.00', '0000-00-00 00:00:00', 'Completed', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
+(5, '0.00', '0000-00-00 00:00:00', 'Completed', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
+(6, '0.00', '2021-11-28 10:25:42', 'Completed', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
+(7, '0.00', '2021-11-28 10:43:15', 'Completed', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
+(8, '45.00', '2021-11-28 10:48:27', 'Completed', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
+(9, '45.00', '2021-11-28 10:50:51', 'Completed', 'Uy', '0708035130', 'uy.nguyen.this@hcmut.edu.vn', '06 Quang Trung'),
+(10, '61.00', '2021-11-28 01:47:49', 'Completed', 'Nguyen Tran Quoc Uy', '0708035130', 'uy@g.c', 'okeoke'),
+(11, '6.00', '2021-11-28 01:50:14', 'Completed', 'Uy', '0777', 'fdf@dfadsf.daf', 'fadsfasdfasdf'),
+(12, '31.00', '2021-11-28 01:55:57', 'Completed', 'aa', '222', 'afa@fad.com', '123546'),
+(13, '36.00', '2021-11-28 01:59:52', 'Completed', 'dfasdf', '0123', 'adfadf@gmil.co', 'fadsfadf'),
+(14, '55.00', '2021-11-28 02:03:08', 'On Delivery', 'adfda', '021', 'dfadsf@dfad.c', 'fadfadsf'),
+(15, '30.00', '2021-11-28 02:04:26', 'On Delivery', 'dfadsfasd', '1321231', 'fdafad@fadf.fadf', 'fadfadsf'),
+(16, '30.00', '2021-11-28 02:06:16', 'Completed', 'dfadsfasd', '1321231', 'fdafad@fadf.fadf', 'fadfadsf'),
+(17, '30.00', '2021-11-28 02:14:31', 'Completed', 'dfadsfasd', '1321231', 'fdafad@fadf.fadf', 'fadfadsf'),
+(18, '30.00', '2021-11-28 02:17:16', 'Completed', 'fad', '123', 'fd@dfasf.com', 'fadsfasdfasd'),
+(19, '11.00', '2021-11-28 02:18:00', 'Completed', 'fdfads', '012321312', 'dfadsfasdf@gmai.com', 'sdfadsfasd'),
+(20, '11.00', '2021-11-28 02:18:34', 'Completed', 'dfadsf', '012', 'fads@ggf.com', 'dfasdfasd'),
+(21, '10.00', '2021-11-28 07:51:11', 'Đang xử lí', 'Nguyen', '0708035130', 'Uy@gm.c', 'Uowy');
 
 --
 -- Indexes for dumped tables
@@ -186,6 +310,18 @@ ALTER TABLE `tbl_food_in_order`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_guess`
+--
+ALTER TABLE `tbl_guess`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_news`
+--
+ALTER TABLE `tbl_news`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
@@ -205,96 +341,37 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tbl_food`
 --
 ALTER TABLE `tbl_food`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `tbl_food_in_order`
 --
 ALTER TABLE `tbl_food_in_order`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `tbl_order`
---
-ALTER TABLE `tbl_order`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:8111
--- Generation Time: Nov 28, 2021 at 12:06 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `restaurant`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_guess`
---
-
-CREATE TABLE `tbl_guess` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `full_name` varchar(150) CHARACTER SET armscii8 NOT NULL,
-  `email` varchar(150) CHARACTER SET armscii8 NOT NULL,
-  `username` varchar(150) CHARACTER SET armscii8 NOT NULL,
-  `address` varchar(255) CHARACTER SET armscii8 NOT NULL,
-  `password` varchar(255) CHARACTER SET armscii8 NOT NULL,
-  `phone` varchar(20) CHARACTER SET armscii8 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tbl_guess`
---
-
-INSERT INTO `tbl_guess` (`id`, `full_name`, `email`, `username`, `address`, `password`, `phone`) VALUES
-(1, '[value-2]', 'Uy', 'test1@bk.co', 'Uowy', 'alo', '0708035130'),
-(2, 'Ravenclaw', 'luanhuynh2001@gmail.com','raven','Fr', '21232f297a57a5a743894a0e4a801fc3','0777123456');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tbl_guess`
---
-ALTER TABLE `tbl_guess`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `tbl_guess`
 --
 ALTER TABLE `tbl_guess`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tbl_news`
+--
+ALTER TABLE `tbl_news`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
