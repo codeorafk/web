871942f652460df24e1cf6ee6f8dd08742359a55
codeorafk <63441848+codeorafk@users.noreply.github.com>
2021-11-29 01:12:01 +0700

    add login

--- view/logout-guess.php
+++ view/logout-guess.php
@@ -3,7 +3,7 @@
     include('../config/constants.php');
     //1. Destory the Session
     session_destroy(); //Unsets $_SESSION['user']
-    unset($_SESSION['user']);
+    unset($_SESSION['guess']);
     //2. REdirect to Login Page
     header('location:'.view);
 ?>
