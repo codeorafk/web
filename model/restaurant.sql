a0c30f47b95f3c7dca4ceaac056a5deef68fb1a5
codeorafk <63441848+codeorafk@users.noreply.github.com>
2021-12-01 16:53:33 +0700

    update db

--- model/restaurant.sql
+++ model/restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:8111
--- Generation Time: Nov 29, 2021 at 09:28 AM
+-- Generation Time: Dec 01, 2021 at 10:52 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -24,6 +24,53 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `image_news`
+--
+
+CREATE TABLE `image_news` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `image_name` text NOT NULL,
+  `section_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `image_news`
+--
+
+INSERT INTO `image_news` (`id`, `image_name`, `section_id`) VALUES
+(1, 'news8.jpg', 1),
+(2, 'news9.jpg', 2),
+(3, 'news8.jpg', 1),
+(4, 'news9.jpg', 2),
+(5, 'news10.jpg', 3),
+(6, 'news11.jpg', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section`
+--
+
+CREATE TABLE `section` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `description` longtext NOT NULL,
+  `title` text NOT NULL,
+  `news_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `section`
+--
+
+INSERT INTO `section` (`id`, `description`, `title`, `news_id`) VALUES
+(1, 'Với tư cách là một gã trai từ Anh Quốc, thì Chelsea boots luôn là một sự lựa chọn không thể nào bỏ qua được. Sỡ hữu tuổi đời kéo dài gần hai thế kỷ, những đôi Chelsea boots đã có sự vinh dự cực kỳ lớn khi được sánh bước cùng với các tầng lớp quý tộc Anh Quốc vào thế kỷ XIX. Để hưởng ứng cái di sản mà tổ tiên xa xưa để lại, Harry luôn chọn những đôi Chelsea boots mỗi khi xuất hiện trước công chúng. Từ những show diễn, sự kiện thảm đỏ hoặc chỉ đơn giản là những ngày xuống phố bình thường, chúng ta hoàn toàn có thể bắt gặp hình ảnh của Harry sánh bước cùng những đôi Chelsea boots. Đi kèm với Chelsea boots luôn là những chiếc quần Skinny Jeans, ở top thì áo và có thể khoác ngoài thêm một chiếc manto hay blazer để phù hợp với thời tiết nhưng vẫn đảm bảo được tính thẩm mỹ nhất định.', 'CHELSEA BOOT', 1),
+(2, 'Nhắc đến Harry Styles thì không thể nào không nhắc đến những đôi Harness boots, đây có thể nói là “best choice” sánh bước cùng anh trong những show diễn và nhiều sự kiện khác. Không thể phủ nhận được hình ảnh của Harness boots trên đôi chân của Harry luôn tạo ra những hiệu ứng mạnh mẽ và đưa items này trở thành “must-have\" items trong tủ đồ của mỗi người. Combo vs Harness luôn là những chiếc textured shirt với nhiều họa tiết bắt mắt và vẫn là quần jeans skinny fits như mọi khi.', 'HARNESS BOOT', 1),
+(3, 'Cũng như những đôi Harness boots, nhưng đổi lại zip boots lại mang một phong thái đơn điệu hơn hẳn. Heeled zip boots luôn được Harry chọn cho những trang phục đơn giản hơn hoặc có thể là những bộ suit trang trọng cho các sự kiện thảm đỏ.', 'HEELED ZIP BOOTS - JODPUR BOOTS', 1),
+(4, 'Trước đây, Thewolf cũng đã có bài viết về Zayn Malik. Hoàn toàn trái ngược với Zayn, hình ảnh của một quý ông lịch lãm nhã nhặn và có đôi chút sự già dặn thì Harry lại chọn cho bản thân một phong cách trẻ trung và phong trần hơn hẵn. Như tiêu đề của bài viết, Harry lại là một gã nghiện boots chính hiệu và bằng chứng cho điều này được thể hiện rất rõ qua ống kính của các paparazi, hầu như Harry luôn mang boots cho mọi hoàn cảnh. Từ những đôi Chelsea boots, Harness boots, zip boots, Jodpur Boots… bất kì hoàn cảnh nào Harry vẫn luôn chọn chúng cho đôi chân của mình.', '', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_admin`
 --
 
@@ -39,6 +86,9 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
+(1, 'Arsenio Leach', 'toduwaxobi', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
+(9, 'Sasha Mendez', 'goxemyde', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
+(10, 'Vijay Thapa', 'vijaythapa', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (12, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
@@ -63,7 +113,7 @@ INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) V
 (4, 'Pizza', 'Food_Category_619.jpg', 'Yes', 'No'),
 (5, 'Burger', 'Food_Category_344.jpg', 'Yes', 'Yes'),
 (6, 'MoMo', 'Food_Category_77.jpg', 'Yes', 'Yes'),
-(13, 'Chicken', 'Food_Category_377.jpg', 'Yes', 'No'),
+(13, 'Chicken', 'Food_Category_377.jpg', 'Yes', 'Yes'),
 (15, 'Beef steak', 'Food_Category_612.jpg', 'Yes', 'Yes');
 
 -- --------------------------------------------------------
@@ -91,7 +141,21 @@ INSERT INTO `tbl_comment` (`id`, `email`, `full_name`, `guess_id`, `news_id`, `c
 (3, 'uy@gmial.co', 'Uy nef', NULL, 1, 'hay lam do xem di moi nguoi\r\nalo alo'),
 (4, 'Uy', 'Nguyen', 1, 1, 'oke'),
 (5, 'test@gmail.com', 'Uy', NULL, 1, 'hello world'),
-(6, 'luu@luu.com', 'another luu', 9, 1, 'test comment from login');
+(6, 'luu@luu.com', 'another luu', 9, 1, 'test comment from login'),
+(7, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(8, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(9, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(10, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(11, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(12, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(13, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(14, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(15, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(16, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(17, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(18, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(19, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke'),
+(20, 'uy.nguyen.this@hcmut.edu.vn', 'uy1', 10, 1, 'oke');
 
 -- --------------------------------------------------------
 
@@ -115,30 +179,30 @@ CREATE TABLE `tbl_food` (
 --
 
 INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
-(5, 'Best chicken', 'Best Firewood Pizza in Town.', '6.00', 'Food-Name-943.jpg', 13, 'No', 'No'),
+(5, 'Best chicken', 'Best Firewood Pizza in Town.', '6.00', 'Food-Name-943.jpg', 13, 'No', 'Yes'),
 (6, 'Sadeko Momo', 'Best Spicy Momo for Winter', '6.00', 'Food-Name-7387.jpg', 8, 'Yes', 'Yes'),
 (7, 'Mixed Pizza', 'Pizza with chicken, Ham, Buff, Mushroom and Vegetables', '10.00', 'Food-Name-7833.jpg', 4, 'Yes', 'No'),
-(9, 'chicken wing', 'ngon, giòn, béo', '12.00', 'Food-Name-830.jpg', 13, 'Yes', 'No'),
-(24, 'Chicken', 'ngon', '10.00', 'Food_Name_756.tmp', 13, 'Yes', 'No'),
-(25, 'Chicken', 'ngon', '10.00', 'Food_Name_331.tmp', 13, 'Yes', 'No'),
-(26, 'Chicken', 'ngon', '10.00', 'Food_Name_28.tmp', 13, 'Yes', 'No'),
-(27, 'Chicken', 'ngon', '10.00', 'Food_Name_16.tmp', 13, 'Yes', 'No'),
-(28, 'Chicken', 'ngon', '10.00', 'Food_Name_866.tmp', 13, 'Yes', 'No'),
-(29, 'Chicken', 'ngon', '10.00', 'Food_Name_76.tmp', 13, 'Yes', 'No'),
-(30, 'Chicken', 'ngon', '10.00', 'Food_Name_640.tmp', 13, 'Yes', 'No'),
-(31, 'Chicken', 'ngon', '10.00', 'Food_Name_308.tmp', 13, 'Yes', 'No'),
-(32, 'Chicken', 'ngon', '10.00', 'Food_Name_415.tmp', 13, 'Yes', 'No'),
-(33, 'Chicken', 'ngon', '10.00', 'Food_Name_266.tmp', 13, 'Yes', 'No'),
-(34, 'Chicken', 'ngon', '10.00', 'Food_Name_998.tmp', 13, 'Yes', 'No'),
-(35, 'Chicken', 'ngon', '10.00', 'Food_Name_988.tmp', 13, 'Yes', 'No'),
-(36, 'Chicken', 'ngon', '10.00', 'Food_Name_27.tmp', 13, 'Yes', 'No'),
-(37, 'Chicken', 'ngon', '10.00', 'Food_Name_280.tmp', 13, 'Yes', 'No'),
-(38, 'Chicken', 'ngon', '10.00', 'Food_Name_928.tmp', 13, 'Yes', 'No'),
-(39, 'Chicken', 'ngon', '10.00', 'Food_Name_608.tmp', 13, 'Yes', 'No'),
-(40, 'Chicken', 'ngon', '10.00', 'Food_Name_915.tmp', 13, 'Yes', 'No'),
-(41, 'Chicken', 'ngon', '10.00', 'Food_Name_850.tmp', 13, 'Yes', 'No'),
-(42, 'Chicken', 'ngon', '10.00', 'Food_Name_261.tmp', 13, 'Yes', 'No'),
-(43, 'Chicken', 'ngon', '10.00', 'Food_Name_539.tmp', 13, 'Yes', 'No'),
+(9, 'chicken wing', 'ngon, giòn, béo', '12.00', 'Food-Name-830.jpg', 13, 'Yes', 'Yes'),
+(24, 'Chicken', 'ngon', '10.00', 'Food_Name_756.tmp', 13, 'Yes', 'Yes'),
+(25, 'Chicken', 'ngon', '10.00', 'Food_Name_331.tmp', 13, 'Yes', 'Yes'),
+(26, 'Chicken', 'ngon', '10.00', 'Food_Name_28.tmp', 13, 'Yes', 'Yes'),
+(27, 'Chicken', 'ngon', '10.00', 'Food_Name_16.tmp', 13, 'Yes', 'Yes'),
+(28, 'Chicken', 'ngon', '10.00', 'Food_Name_866.tmp', 13, 'Yes', 'Yes'),
+(29, 'Chicken', 'ngon', '10.00', 'Food_Name_76.tmp', 13, 'Yes', 'Yes'),
+(30, 'Chicken', 'ngon', '10.00', 'Food_Name_640.tmp', 13, 'Yes', 'Yes'),
+(31, 'Chicken', 'ngon', '10.00', 'Food_Name_308.tmp', 13, 'Yes', 'Yes'),
+(32, 'Chicken', 'ngon', '10.00', 'Food_Name_415.tmp', 13, 'Yes', 'Yes'),
+(33, 'Chicken', 'ngon', '10.00', 'Food_Name_266.tmp', 13, 'Yes', 'Yes'),
+(34, 'Chicken', 'ngon', '10.00', 'Food_Name_998.tmp', 13, 'Yes', 'Yes'),
+(35, 'Chicken', 'ngon', '10.00', 'Food_Name_988.tmp', 13, 'Yes', 'Yes'),
+(36, 'Chicken', 'ngon', '10.00', 'Food_Name_27.tmp', 13, 'Yes', 'Yes'),
+(37, 'Chicken', 'ngon', '10.00', 'Food_Name_280.tmp', 13, 'Yes', 'Yes'),
+(38, 'Chicken', 'ngon', '10.00', 'Food_Name_928.tmp', 13, 'Yes', 'Yes'),
+(39, 'Chicken', 'ngon', '10.00', 'Food_Name_608.tmp', 13, 'Yes', 'Yes'),
+(40, 'Chicken', 'ngon', '10.00', 'Food_Name_915.tmp', 13, 'Yes', 'Yes'),
+(41, 'Chicken', 'ngon', '10.00', 'Food_Name_850.tmp', 13, 'Yes', 'Yes'),
+(42, 'Chicken', 'ngon', '10.00', 'Food_Name_261.tmp', 13, 'Yes', 'Yes'),
+(43, 'Chicken', 'ngon', '10.00', 'Food_Name_539.tmp', 13, 'Yes', 'Yes'),
 (44, 'Best sell Beef steak', 'ngon như Uy', '1.00', 'default.jpg', 15, 'Yes', 'Yes'),
 (45, 'uydeptrai', 'uy la deptrai nhat', '12.00', 'default.jpg', 15, 'Yes', 'Yes'),
 (46, 'beef', 'beo ngon', '12.00', 'Food_Name_959.tmp', 15, 'Yes', 'Yes');
@@ -242,11 +306,8 @@ CREATE TABLE `tbl_guess` (
 --
 
 INSERT INTO `tbl_guess` (`id`, `full_name`, `email`, `username`, `address`, `password`, `phone`, `status`) VALUES
-(1, 'Nguyen', 'Uy', 'uy', 'Uowy', '21232f297a57a5a743894a0e4a801fc3', '0708035130', 'Sleeping'),
-(2, 'Luan', 'luan@bk.co', 'luan', 'Luan\'s address', 'dd960d42bb47da21af3b3b0c31684540', '0708050102', 'Sleeping'),
-(3, 'Luu', 'luu@bk.co', 'luu', 'luwu @ uwu ', 'b082b04c7a891a7f67d2400c2688fa06', '0123456789', 'Sleeping'),
-(4, 'Khoa', 'khoa@bk.co', 'khoa', 'khoa khooa khhoa kkhoa khoaa', 'e8ed38edd226e6d0bb0f65e07d8f3f55', '0123452785', 'Sleeping'),
-(7, 'Another Uy', 'uy@gma.com', 'uy1', '06 Quang Trung P Binh Dinh', '28a91aaba26255e090f348abc10f0890', '0123456789', 'Sleeping');
+(7, 'Uy', 'Uy@gmail.com', 'uy', '06 Quang Trung', '28a91aaba26255e090f348abc10f0890', '0708035130', 'Active'),
+(10, 'uy1', 'uy.nguyen.this@hcmut.edu.vn', 'uy1', '06 Quang Trung', '28a91aaba26255e090f348abc10f0890', '0708035130', 'Active');
 
 -- --------------------------------------------------------
 
@@ -267,8 +328,8 @@ CREATE TABLE `tbl_news` (
 --
 
 INSERT INTO `tbl_news` (`id`, `description`, `title`, `image_name`, `status`) VALUES
-(1, 'Some things in this world are largely disappointing, i.e. the supermarket being sold out of special-edition sweets, ice cream falling on the ground, spaghetti sauce on a white shirt. And Im sorry to trigger that disappointment once again. Get this: Taco Bell, the iconic fast food haven, is CLOSED on Thanksgiving Day. That means if you want to eat delicious tacos and nachos on Thanksgiving, you will have to make them yourself. Yeah, that is a scary thought. Seriously though, we’ll need a minute to gather ourselves. After all, Taco Bell has been there for us through thick and thin, drunk eats and nacho cravings galore. And now, burritos and tacos are deserting us in our moment of need. Because let’s face it: We’ll inevitably overcook the Thanksgiving turkey. Sigh. But everyone deserves a day off, especially the angels who craft Doritos Locos Tacos. Tears aside, not all hope should be lost. While the chain is traditionally closed for Thanksgiving (double check with your local store to see if its hours are different—it is also a good idea to check in re: their most up-to-date COVID-19 safety protocols), the restaurant will open its doors once again starting on Black Friday. You know where to find us on November 26 (...at the Taco Bell drive-thru in case that was not clear)', 'Taco Bell For Thanksgiving Is A Major Mood-But Is It Open?', 'news1.jpg', 'Active'),
-(2, 'You know what happens to the best-laid plans, right? They go awry. Meaning that Thanksgiving shopping list you triple-checked actually had something missing—and you probably will not notice it until the morning of the holiday. Luckily, a whole slew of supermarket chains have bet on the fact that you will do just that, and they are keeping their doors open for last-minute purchases. Hours are subject to change regionally, especially given the ongoing COVID-19 pandemic, so be sure to call your closest location before running out. Here is a general guide to the grocery stores you can count on come Thanksgiving day—and the ones that will not be open, including some that are closing down for the holiday for the first time in years. P.S. Not up for cooking this year? These fast food joints will stay open on Thanksgiving, and you can sit down for a meal at these restaurants on Turkey Day, too.', 'All The Grocery Stores Open On Thanksgiving Day, Because You Know You are Going To Forget Something', 'tksgiving.jfif', 'Active');
+(1, 'Some things in this world are largely disappointing, i.e. the supermarket being sold out of special-edition sweets, ice cream falling on the ground, spaghetti sauce on a white shirt. And Im sorry to trigger that disappointment once again. Get this: Taco Bell, the iconic fast food haven, is CLOSED on Thanksgiving Day. That means if you want to eat delicious tacos and nachos on Thanksgiving, you will have to make them yourself. Yeah, that is a scary thought. Seriously though, we’ll need a minute to gather ourselves. After all, Taco Bell has been there for us through thick and thin, drunk eats and nacho cravings galore. And now, burritos and tacos are deserting us in our moment of need. Because let’s face it: We’ll inevitably overcook the Thanksgiving turkey. Sigh. But everyone deserves a day off, especially the angels who craft Doritos Locos Tacos. Tears aside, not all hope should be lost. While the chain is traditionally closed for Thanksgiving (double check with your local store to see if its hours are different—it is also a good idea to check in re: their most up-to-date COVID-19 safety protocols), the restaurant will open its doors once again starting on Black Friday. You know where to find us on November 26 (...at the Taco Bell drive-thru in case that was not clear)', 'Taco Bell For Thanksgiving Is A Major Mood-But Is It Open?', 'tksgiving.jfif', 'Active'),
+(2, 'You know what happens to the best-laid plans, right? They go awry. Meaning that Thanksgiving shopping list you triple-checked actually had something missing—and you probably will not notice it until the morning of the holiday. Luckily, a whole slew of supermarket chains have bet on the fact that you will do just that, and they are keeping their doors open for last-minute purchases. Hours are subject to change regionally, especially given the ongoing COVID-19 pandemic, so be sure to call your closest location before running out. Here is a general guide to the grocery stores you can count on come Thanksgiving day—and the ones that will not be open, including some that are closing down for the holiday for the first time in years. P.S. Not up for cooking this year? These fast food joints will stay open on Thanksgiving, and you can sit down for a meal at these restaurants on Turkey Day, too.', 'All The Grocery Stores Open On Thanksgiving Day, Because You Know You are Going To Forget Something', 'new2.jpg', 'Active');
 
 -- --------------------------------------------------------
 
@@ -323,6 +384,18 @@ INSERT INTO `tbl_order` (`id`, `total`, `order_date`, `status`, `customer_name`,
 --
 
 --
+-- Indexes for table `image_news`
+--
+ALTER TABLE `image_news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `section`
+--
+ALTER TABLE `section`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
@@ -375,6 +448,18 @@ ALTER TABLE `tbl_order`
 --
 
 --
+-- AUTO_INCREMENT for table `image_news`
+--
+ALTER TABLE `image_news`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `section`
+--
+ALTER TABLE `section`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
@@ -390,7 +475,7 @@ ALTER TABLE `tbl_category`
 -- AUTO_INCREMENT for table `tbl_comment`
 --
 ALTER TABLE `tbl_comment`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `tbl_food`
@@ -408,7 +493,7 @@ ALTER TABLE `tbl_food_in_order`
 -- AUTO_INCREMENT for table `tbl_guess`
 --
 ALTER TABLE `tbl_guess`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbl_news`
